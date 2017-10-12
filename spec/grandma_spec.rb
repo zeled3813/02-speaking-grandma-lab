ec25d1ca86eee5bcf015edf12cd4c30730343e8d
zeled3813 <zeled3813@hstat.org>
2017-10-12 15:44:25 +0000

    done

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -28,3 +28,13 @@ describe '#speak_to_grandma' do
   end
 
 end
+
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
